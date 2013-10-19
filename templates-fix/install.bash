101f0cdd3e24edd279472295a37b35cf154a706e
phr34kz <phr34kzy@outlook.com>
2013-10-19 12:57:28 +0800

    + ‘pybombs-dl’ + ‘recipe-fix/armadillo.lwr’ + ‘recipe-fix/boost.lwr’ + ‘recipe-fix/cmake.lwr’ + ‘recipe-fix/gnuradio.lwr’ + ‘recipe-fix/gqrx.lwr’ + ‘recipe-fix/gr-smartnet.lwr’ + ‘recipe-fix/pysstv.lwr’ + ‘recipe-fix/slowrx.lwr’ + ‘recipe-fix/swig.lwr’ ...

---
+++ templates-fix/install.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+clear
+
+echo "----------------------------------------"
+echo "                                        "
+echo "        {[   Evilzone Network   ]}      "
+echo "                                        "
+echo "                  Welcome               "
+echo "                                        "
+echo "----------------------------------------"
+sleep 1
+
+configuring()
+{
+echo ""
+echo -n "Username for GIT access? : "
+read gituser1
+echo ""
+echo -n "Where do you want gnuradio to be installed at? Example - /root/Desktop/gnuradio : "
+read prefix1
+echo ""
+echo -n "Do you want gnuradio to install from source, deb package or both? Example - deb,src : "
+read satisfy1
+echo ""
+echo -n "Time the monitor thread waits (in seconds) before retrying downloads - Best time is 30 : "
+read time1
+
+touch config.dat
+echo "[config]" > config.dat
+echo "gituser = $gituser1" >> config.dat
+echo "prefix = $prefix1" >> config.dat
+echo "satisfy_order = $satisfy1"  >> config.dat
+echo "timeout = $time1" >> config.dat
+echo "cmakebuildtype = RelWithDebInfo" >> config.dat
+}
+
+if [ ! -f config.dat ]; then
+	echo "config.dat exists! continuing"
+	sleep 3
+	configuring
+fi
+
+./pybombs install gnuradio
